b0515baaa5a8962b555f26b1bdff8c9f3ce3bc6e
BALDE <79581163+BaldeAl@users.noreply.github.com>
2024-04-30 13:49:16 +0200

    handbrake

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM squidfunk/mkdocs-material
-
-RUN mkdir /docs/docs
-ADD README.md /docs/README.md
-ADD docs/mkdocs.yml /docs/mkdocs.yml
-
-WORKDIR /docs
--- sources/realWorldApp-Conduit-vue3/src/components/EditArticleButton.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-defineProps<{ to: string }>()
-</script>
-
-<template>
-  <router-link :to="to" class="btn btn-outline-secondary btn-sm space">
-    <i class="ion-edit space" /> Edit Article
-  </router-link>
-</template>
